9b8a0eb0a004746b17fddf04ce2997fe67084a98
ericperry95 <37937608+ericperry95@users.noreply.github.com>
2020-09-09 10:12:29 -0400

    Add files via upload

---
+++ BarChart.java
@@ -0,0 +1,55 @@
+// chapter 6 exersize
+import java.util.Scanner;
+
+public class BarChart
+{
+	public static void main(String[] args) 
+	{
+		// constants and variables
+		int artPoints;
+		int bobPoints;
+		int calPoints;
+		int danPoints;		
+		int eliPoints;
+
+		// input phase
+		Scanner input = new Scanner(System.in);
+		System.out.print("Please enter points earned by Art >>");
+		artPoints = input.nextInt();
+
+		System.out.print("Please enter points earned by Bob >>");
+		bobPoints = input.nextInt();
+
+		System.out.print("Please enter points earned by Cal >>");
+		calPoints = input.nextInt();
+
+		System.out.print("Please enter points earned by Dan >>");
+		danPoints = input.nextInt();
+
+		System.out.print("Please enter points earned by Eli >>");
+		eliPoints = input.nextInt();
+
+		// output phase
+		System.out.println();
+		System.out.println("Points earned this game");
+		displayPoints("Art", artPoints);
+		displayPoints("Bob", bobPoints);
+		displayPoints("Cal", calPoints);
+		displayPoints("Dan", danPoints);
+		displayPoints("Eli", eliPoints);		
+	}
+
+	// method that generates the asterisk characters for the bar chart
+
+	public static void displayPoints(String name, int points)
+	{
+		System.out.print("\n" + name + ": ");
+
+		for(int x = 1; x <= points; ++x)
+		{
+			System.out.print("*");
+
+		}
+
+	}
+}
